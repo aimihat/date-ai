dc459f9b0c0d8e128f4b5e6912ebf2dd8b081b76
aj1212 <aj1212@ic.ac.uk>
2018-01-28 04:38:29 +0000

    Hard-coding

--- Webapp/chatbot.py
+++ Webapp/chatbot.py
@@ -13,7 +13,13 @@ from chatterbot.trainers import ListTrainer
 from chatterbot import ChatBot
 
 hard_coded = {
-    'hey': 'hi there'
+    'hey': 'hi there',
+    'how are you':"I am good, thanks. How are you?",
+    "what is your name":"My name is Shreya. What's your name?",
+    "what are you working":"I am working on my compiler project.",
+    "did you go to the class yesterday":"Yes, I did. It was a very interesting class. I think the professor is really good",
+    "what is your phone number":"I don't have a phone. Sorry!",
+    "how old are you":"I am 20 years old"
 }
 
 def set_bot():
